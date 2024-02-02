8701be429e1c80346be0d90192948785df459c69
apmenicucci <adam.menicucci@salesianum.org>
2024-02-02 13:55:30 -0500

    .

--- ArrayPractice.java
+++ ArrayPractice.java
@@ -2,6 +2,7 @@ public class ArrayPractice {
     public static void main(String[] args) {
         String[] stringArray = new String[4];
         int[] intArray = {1, 2, 3, 4};
-        
+        System.out.println(stringArray);
+        System.out.println(intArray);
     }
 }
